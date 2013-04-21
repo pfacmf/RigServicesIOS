cbe8ed622196785c4f210064ce521bfb59e1e275
florin.cazacu@gmail.com <florin.cazacu@gmail.com>
2013-04-21 14:48:48 -0500

    delete documents

--- RigServicesIOS3/scripts/documentlist.js
+++ RigServicesIOS3/scripts/documentlist.js
@@ -1,4 +1,5 @@
 var documentListSelector;
+var documentType;
 
 function onDrawingsListShow() {
 	onDocumentListShow("Drawings", "#drawings-list-view");
@@ -21,4 +22,31 @@ function onGetDocumentListSuccess(data, status) {
 	if (docList) {
 		$(documentListSelector).data("kendoMobileListView").setDataSource(new kendo.data.DataSource({data:docList}));
 	}
+}
+
+function onDrawingsClick(e) {
+	documentListSelector = '#drawings-list-view';
+    documentType = 'Drawings';
+	onDocumentClick(e);
+}
+function onWorkOrderClick(e) {
+	documentListSelector = '#work-order-list-view';
+    documentType = 'Work Order';
+	onDocumentClick(e);
+}
+function onDocumentClick(e) {
+	$("#document-menu").data("kendoMobileActionSheet").open(e.item, e.dataItem.id);
+}
+
+function deleteDocument(e) {
+	if (confirm("Are you sure you want to delete the document?")) {
+		var url = getRequestURL('deletedocument');
+		url += "&" + $.param({documentId: e.context});
+		$.get(url, onDeleteDocumentSuccess).fail(onRequestFail);
+	}
+}
+
+function onDeleteDocumentSuccess() {
+    onDocumentListShow(documentType, documentListSelector);
+    alert("Document was deleted");
 }

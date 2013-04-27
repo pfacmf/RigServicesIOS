c0f037c61d924694f8b6b16360d7ab973f289785
florin.cazacu@gmail.com <florin.cazacu@gmail.com>
2013-04-27 08:25:59 -0500

    Delete Crew

--- RigServicesIOS3/scripts/crewlist.js
+++ RigServicesIOS3/scripts/crewlist.js
@@ -1,4 +1,4 @@
-function onCrewListShow(type, listSelector) {
+function onCrewListShow() {
 	setRigTabsTitle()
 	var url = getRequestURL('crewlist');
 	showLoader("Fetching crew list", $.get(url, onGetCrewListSuccess).fail(onRequestFail));
@@ -13,4 +13,25 @@ function onGetCrewListSuccess(data, status) {
 	if (list) {
 		$("#crew-list-view").data("kendoMobileListView").setDataSource(new kendo.data.DataSource({data:list}));
 	}
+}
+
+function onCrewClick(e) {
+	$("#crew-menu").data("kendoMobileActionSheet").open(e.item, e.dataItem);
+}
+
+function deleteCrew(e) {
+	if (confirm("Are you sure you want to delete the crew member?")) {
+		var url = getRequestURL('deletecrew');
+		url += "&" + $.param({crewId: e.context.id});
+		showLoader("Deleting crew member", $.get(url, onDeleteCrewSuccess).fail(onRequestFail));
+	}
+}
+
+function onDeleteCrewSuccess() {
+	if (App.isLoaderCanceled()) {
+		return;
+	}
+    hideLoader();
+	onCrewListShow();
+	alert("Crew member was deleted");
 }
